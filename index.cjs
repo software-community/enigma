789244a71a9803a8ccb2201b33605d277439cf06
Arpit <arpit01goel@gmail.com>
2025-01-22 17:43:09 +0530

    integrated Madhav's code

--- index.cjs
+++ index.cjs
@@ -13,6 +13,20 @@ app.use(express.static(path.join(__dirname,"public")));
 app.route("/dashboard").get(async(req,res)=>{
     res.render("dashboard");
 })
+app.route("/account").get(async (req,res)=>{
+    res.render("account",{
+        Name: "Dummy",
+        Email: "dummy@example.com",
+        quizzes: [
+            { name: "somequiz", date: "yesterday", score: "10/10", rank: "1/100" },
+            { name: "somequiz", date: "yesterday", score: "10/10", rank: "1/100" }
+        ],
+        hostedQuizzes: [
+            { name: "quiz 1", date: "6 January 2024", participants: 122 }
+        ]
+    
+    })
+})
 
 
 app.listen(PORT,console.log("server started at port: ", PORT))
